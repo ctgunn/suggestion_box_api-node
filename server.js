b576d941b5d4204fcec31c835718e48ea336f26c
Cliff Gunn <cliff.gunn@gmail.com>
2022-02-04 02:09:34 -0600

    Updating packages.

--- server.js
+++ server.js
@@ -21,5 +21,6 @@ app.listen(port);
 console.log('todo list RESTful API server started on: ' + port);
 
 app.use(function(req, res) {
-    res.status(404).send({url: req.originalUrl + ' not found'})
+    res.status(404).send({url: req.originalUrl + ' not found.'});
+    // res.status(403).send({url: req.originalUrl + ' - Permission denied.'});
 });
